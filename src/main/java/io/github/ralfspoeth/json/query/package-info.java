c1c4b5082e63400edd2bc58401cc012f4176eb30
ralfspoeth <ralf.spoeth@gmail.com>
2026-08-26 15:37:03 +0200

    reference to the upcoming JEP 540 included

--- src/main/java/io/github/ralfspoeth/json/query/package-info.java
+++ src/main/java/io/github/ralfspoeth/json/query/package-info.java
@@ -1,9 +1,11 @@
 /**
  * Designed to make querying {@link io.github.ralfspoeth.json.data.JsonValue}s simpler.
- * Here is how you do it without
- * {@snippet class="GreysonWay" region="classic"}
+ * The OpenJDK community has an upcoming JEP 540 incubating a <em>Simple JSON API</em>
+ * with an example how read weather forecast data
+ * {@linkplain "<a href="https://openjdk.org/jeps/540#appendix">here</a>"}
+ * Here is how you do the same with Greyson without the Query API
+ * {@snippet class = "GreysonWay" region = "classic"}
  * and here with {@link Selector}s and {@link Pointer}s.
- * {@snippet class="GreysonWay" region="query"}
- *
+ * {@snippet class = "GreysonWay" region = "query"}
  */
 package io.github.ralfspoeth.json.query;
