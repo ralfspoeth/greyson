66a4d5fc7e5b962b0e7b0197b93598620b56a181
ralfspoeth <ralf.spoeth@gmail.com>
2026-09-15 10:29:48 +0200

    grammar and types in the package-info

--- src/main/java/io/github/ralfspoeth/json/query/package-info.java
+++ src/main/java/io/github/ralfspoeth/json/query/package-info.java
@@ -1,8 +1,8 @@
 /**
  * Designed to make querying {@link io.github.ralfspoeth.json.data.JsonValue}s simpler.
  * The OpenJDK community has an upcoming JEP 540 incubating a <em>Simple JSON API</em>
- * with an example how read weather forecast data
- * "<a href="https://openjdk.org/jeps/540#appendix">here</a>
+ * with an example how to read weather forecast data
+ * <a href="https://openjdk.org/jeps/540#appendix">here</a>.
  * Here is how you do the same with Greyson without the Query API
  * {@snippet class = "GreysonWay" region = "classic"}
  * and here with {@link Selector}s and {@link Pointer}s.
